a1a13054289e007cbb4080f56392beb6216ac3fb
shreya123-456 <155753091+shreya123-456@users.noreply.github.com>
2025-07-07 20:21:31 +0530

    Create SpringLearnApplication.java

---
+++ Week-4/Spring-Rest-Handson/SpringWeb_Project/SpringLearnApplication.java
@@ -0,0 +1,19 @@
+package com.cognizant.spring_learn;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+    //  Logger declaration
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLearnApplication.class, args);
+        //  Log message
+        LOGGER.info("Inside main");
+    }
+}
