a1a91038f781d875b601722dd668a0ceb35b2975
shreya123-456 <155753091+shreya123-456@users.noreply.github.com>
2025-07-26 19:36:43 +0530

    Add files via upload

---
+++ Week-7/Exercise-9/EvenPlayers.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const EvenPlayers = ({ team }) => {
+  const [, second, , fourth, , sixth] = team;
+  return (
+    <ul>
+      <li>Second : {second}</li>
+      <li>Fourth : {fourth}</li>
+      <li>Sixth : {sixth}</li>
+    </ul>
+  );
+};
+
+export default EvenPlayers;
