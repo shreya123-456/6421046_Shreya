ad92d285051e5db1a498c128e5baebd833060643
shreya123-456 <155753091+shreya123-456@users.noreply.github.com>
2025-07-27 12:20:21 +0530

    Add files via upload

---
+++ Week-7/Exercise-12/App.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import Greeting from './components/Greeting';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLoginClick = () => setIsLoggedIn(true);
+  const handleLogoutClick = () => setIsLoggedIn(false);
+
+  let button;
+  if (isLoggedIn) {
+    button = <LogoutButton onClick={handleLogoutClick} />;
+  } else {
+    button = <LoginButton onClick={handleLoginClick} />;
+  }
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '10%' }}>
+      <Greeting isLoggedIn={isLoggedIn} />
+      {button}
+    </div>
+  );
+}
+
+export default App;
