559b50de6d3ca20ee52a748d68a0179b5e6bb62c
shreya123-456 <155753091+shreya123-456@users.noreply.github.com>
2025-07-30 19:07:32 +0530

    Update Setup.sh

--- Week-8/Exercise-1/Setup.sh
+++ Week-8/Exercise-1/Setup.sh
@@ -1,4 +1,19 @@
-git --version
+#!/bin/bash
+
+>>git --version 
+git version 2.41.0.windows.1
+
 git config --global user.name "Shreya"
 git config --global user.email "Shreya6@gmail.com"
-git config --list
+git config --global core.editor "notepad++"
+
+mkdir GitDemo
+cd GitDemo
+git init
+
+echo "Welcome to Git Hands-On Lab!" > welcome.txt
+
+git add welcome.txt
+
+git commit -m "Initial commit with welcome.txt"
+
