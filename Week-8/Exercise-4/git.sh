c86aec33f0b751c3dbdf69f3238eebfe91ce458f
shreya123-456 <155753091+shreya123-456@users.noreply.github.com>
2025-08-01 10:35:49 +0530

    Update git.sh

--- Week-8/Exercise-4/git.sh
+++ Week-8/Exercise-4/git.sh
@@ -1,1 +1,18 @@
+git config --global user.name "Shreya"
+git config --global user.email "Shreya6@gmail.com"
 
+git checkout master
+git status
+echo "<message>Hello from branch</message>" > hello.xml
+git add hello.xml
+git commit -m "Added hello.xml in GitWork"
+git checkout master
+echo "<message>Hello from master</message>" > hello.xml
+git add hello.xml
+git commit -m "Added hello.xml in master"
+git log --oneline --graph --decorate --all
+git diff GitWork
+
+git merge GitWork
+
+cat hello.xml
