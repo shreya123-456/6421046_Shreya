4543b6940735eee725365cc2437b39a59b6e92ad
shreya123-456 <155753091+shreya123-456@users.noreply.github.com>
2025-08-01 10:20:33 +0530

    Create git.sh

---
+++ Week-8/Exercise-2/git.sh
@@ -0,0 +1,9 @@
+git status
+
+git add .gitignore
+git commit -m "Added .gitignore to ignore .log files and log folder"
+
+git status
+#Output:
+#On branch main
+#nothing to commit, working tree clean
